4ab736e62168a81431d817ca8f71a477650b65fa
Nick Arnold <the_narno@yahoo.com>
2021-05-12 14:46:05 -0400

    added hidden files in assignment 1

--- src/main/java/com/maven1/definitions/Definitions.java
+++ src/main/java/com/maven1/definitions/Definitions.java
@@ -5,6 +5,7 @@ public class Definitions {
 	private String statementOfIntent;
 	private String somethingNice;
 	
+	//You can use these to different class types (a car with 4 wheels, a trycicle with 3)
 	public Definitions() {
 		this.statementOfIntent = "We would just like to say";
 		this.somethingNice = " we have a great instructor!";
